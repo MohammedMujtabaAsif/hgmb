f16c058161a006ce53c1407dd470a5ce23dea742
MohammedMujtabaAsif <asifmm@aston.ac.uk>
2020-03-22 02:55:22 +0000

    StreamBuilder and Pull-to-refresh

--- lib/pages/explore.dart
+++ lib/pages/explore.dart
@@ -1,175 +1,94 @@
-import 'dart:convert';
+import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:hgmb/pages/userProfilePublic.dart';
-import 'package:hgmb/utils/userProfile.dart';
+import 'package:hgmb/utils/userListBox.dart';
 import 'package:hgmb/utils/databaseHelper.dart';
+import 'package:hgmb/utils/userProfile.dart';
 
 class ExplorePage extends StatefulWidget {
   @override
-  State createState() => new ExplorePageState();
+  State createState() => new _ExplorePageState();
 }
 
-class ExplorePageState extends State<ExplorePage> {
-  var db = new DatabaseHelper();
-  Future users;
+class _ExplorePageState extends State<ExplorePage>
+    with AutomaticKeepAliveClientMixin<ExplorePage> {
+  // Future users;
+  StreamController<List<User>> _users;
+
+  @override
+  bool get wantKeepAlive => true;
 
   @override
   void initState() {
     super.initState();
-    users = _getUsers();
+    _users = StreamController<List<User>>();
+    _getUsers();
   }
 
   _getUsers() async {
-    var response = await db.getData('/allUsers');
-    var body = json.decode(response.body);
-    if (body != null) {
-      List<User> _usersList = new List();
-      int index = 0;
-      while (index < body.length) {
-        User _user = new User.fromJson(body[index]);
-        if (_user != null) {
-          _usersList.add(_user);
-          print("user " + (_user.prefName).toString() + " added");
-        } else {
-          print("user is null");
-        }
-        index++;
-      }
-      return _usersList;
-    }
+    var db = new DatabaseHelper();
+    var users = await db.getUsersAsList('/allUsers');
+    setState(() {
+      _users.add(users);
+    });
+    // return users;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _users.close();
   }
 
   Widget build(BuildContext context) {
-    final mediaWidth = MediaQuery.of(context).size.width;
+    super.build(context);
     return new Scaffold(
-      body: FutureBuilder(
-        future: users,
+      body: StreamBuilder(
+        stream: _users.stream,
         builder: (context, snapshot) {
-          return snapshot.hasData
-              ? ListView.builder(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return new Padding(
-                      padding: EdgeInsets.fromLTRB(5.0, 10.0, 5.0, 0.0),
-                      child: Column(children: <Widget>[
-                        new UserListBox(
-                          mediaWidth: mediaWidth,
+          if (snapshot.hasData == true && snapshot.data.length > 0) {
+            return new RefreshIndicator(
+              child: Column(
+                children: <Widget>[
+                  Expanded(
+                      child: ListView.builder(
+                    // scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        padding: EdgeInsets.fromLTRB(5.0, 10.0, 5.0, 0.0),
+                        child: new UserListBox(
+                          id: snapshot.data[index].id.toString(),
                           prefName: snapshot.data[index].prefName,
                           maritalStatus: snapshot.data[index].maritalStatus,
                           city: snapshot.data[index].city,
+                          buttonName: "View Profile",
+                          buttonMethod: () {
+                            Navigator.of(context).pushNamed(
+                                '/userProfilePublic',
+                                arguments: snapshot.data[index]);
+                          },
                         ),
-                      ]),
-                    );
-                  },
-                )
-              : new Center(
-                  child: Text(
-                  "Loading Users",
-                  // textAlign: TextAlign.center,
-                ));
+                      );
+                    },
+                  ))
+                ],
+              ),
+              onRefresh: () => _getUsers(),
+            );
+          } else {
+            return new Center(
+              child: Text(
+                snapshot.hasData == true && snapshot.data.length == 0
+                    ? "No Users"
+                    : "Loading Users",
+              ),
+            );
+          }
         },
       ),
     );
-  }
-}
-
-class UserListBox extends StatefulWidget {
-  final double mediaWidth;
-  final String prefName;
-  final String city;
-  final String maritalStatus;
-  final String imageLocation;
-
-  const UserListBox(
-      {Key key,
-      this.mediaWidth,
-      this.prefName,
-      this.city,
-      this.maritalStatus,
-      this.imageLocation})
-      : super(key: key);
-
-  @override
-  UserListBoxState createState() => UserListBoxState();
-}
-
-class UserListBoxState extends State<UserListBox> {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-        padding: EdgeInsets.all(5.0),
-        child: Container(
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(10.0)),
-            padding: EdgeInsets.fromLTRB(5.0, 0.0, 5.0, 0.0),
-            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: <
-                Widget>[
-              Expanded(
-                  flex: 1,
-                  child: Padding(
-                      padding: EdgeInsets.all(5.0),
-                      child: Container(
-                          child: widget.imageLocation == null
-                              ? ClipRRect(
-                                  borderRadius: BorderRadius.circular(20.0),
-                                  child: Image.network(
-                                    'https://www.thispersondoesnotexist.com/image',
-                                    fit: BoxFit.fill,
-                                  ))
-                              : ClipRRect(
-                                  borderRadius: BorderRadius.circular(200.0),
-                                  child: Image.network(
-                                    widget.imageLocation,
-                                    fit: BoxFit.cover,
-                                  ))))),
-              Expanded(
-                  flex: 2,
-                  child: Column(
-                      // mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.all(2.0),
-                          child: Text(widget.prefName,
-                              style: TextStyle(
-                                  // decoration: TextDecoration.underline,
-                                  )),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(2.0),
-                          child: Text(widget.city,
-                              style: TextStyle(
-                                  // decoration: TextDecoration.underline,
-                                  )),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(2.0),
-                          child: Text(widget.maritalStatus,
-                              style: TextStyle(
-                                  // decoration: TextDecoration.underline,
-                                  )),
-                        ),
-                      ])),
-              Expanded(
-                  flex: 1,
-                  child: FittedBox(
-                      fit: BoxFit.cover,
-                      child: FlatButton(
-                        child: Text("View Profile"),
-                        color: Colors.blue,
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              new MaterialPageRoute(
-                                  builder: (context) => UserProfilePublic(
-                                        prefName: widget.prefName,
-                                        city: widget.city,
-                                        maritalStatus: widget.maritalStatus,
-                                      )));
-                        },
-                      ))),
-            ])));
   }
 }
